3091096f8514bb693eb2e6e843198b27861a9919
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-13 17:21:41 +0000

    Complete key_for_min_value

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_key = nil
+  sum = 1000000
+  name_hash.each do |name, value|
+    if value < sum
+      min_key = name
+      sum = value
+    end
+  end
+  min_key
 end
